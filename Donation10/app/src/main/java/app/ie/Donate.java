cc46b71da44b2796488eaba14b9c09b43b922925
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2021-03-06 08:40:13 +0700

    first commit

---
+++ Donation10/app/src/main/java/app/ie/Donate.java
@@ -0,0 +1,14 @@
+package app.ie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Donate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+    }
+}
